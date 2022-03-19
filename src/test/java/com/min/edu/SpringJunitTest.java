081a287cd7658a551fe01570bcb672f39ee0624a
DESKTOP-QA0M42J\taewan <ntw2100@gmail.com>
2022-03-19 19:54:21 +0900

    구디 수업 데모

---
+++ src/test/java/com/min/edu/SpringJunitTest.java
@@ -0,0 +1,15 @@
+package com.min.edu;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SpringJunitTest {
+
+	@Test
+	public void test() { 
+	}
+
+}
